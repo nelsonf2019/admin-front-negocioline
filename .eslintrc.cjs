597b13a8fa2c4d14097c843bff8305cf19ff31d4
nelsonf2019 <nelsonfercher@gmail.com>
2024-04-01 21:58:02 -0300

    corrigiedo eslintrc

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -26,9 +26,7 @@ const config = {
     "@typescript-eslint/no-unused-vars": "off",
     "@typescript-eslint/no-unsafe-return":"off",
     "@typescript-eslint/no-unsafe-member-access":"off",
-    "@typescript-eslint/no-unsafe-call":"off",
-    
-    
+    "@typescript-eslint/no-unsafe-call":"off",  
   },
 };
 
